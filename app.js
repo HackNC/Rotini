fb4828cfd780b3f7cabb847a244fc9c2b919f2eb
Thi Le <thi.le@unc.edu>
2021-09-19 16:59:58 -0400

    create table

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4201;
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database.db');
+
+app.use(express.json()); 
+
+app.get('/', (req, res) => { 
+    db.serialize(function() {
+      
+        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        for (var i = 0; i < 10; i++) {
+            stmt.run("Ipsum " + i);
+        }
+        stmt.finalize();
+      
+        db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+            console.log(row.id + ": " + row.info);
+        });
+      });
+      
+      db.close();
+    res.send({kq: 1})
+ });
+
+app.listen(PORT, () => {
+    console.log(`this is port ${PORT}`)
+})
