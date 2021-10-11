5f131ae95a7a719f74dd97339daa28003a9169b0
Thi Le <thi.le@unc.edu>
2021-10-10 20:07:49 -0400

    add edit function to table and qrcode

--- controllers/form_controllers.js
+++ controllers/form_controllers.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const db = require('../database')
 
+const QRCode = require('qrcode')
+
+
 router.get('/addHackerForm', (req, res) => {
     res.sendFile(__dirname + "/addHackerForm.html")
 })
@@ -33,6 +36,47 @@ router.post('/addEvent', (req, res) => {
             res.redirect('/table/eventTable')
         }
     )
+})
+
+router.get('/hacker/:id/delete', (req, res) => {
+    const id = req.params.id
+    db.run(`DELETE FROM hackerTable WHERE id=?`, [id], () => {
+        res.redirect('/table/hackerTable')
+    })
+})
+
+router.get('/hacker/:id/profile', (req, res) => {
+    const id = req.params.id
+    db.all(
+        `SELECT * FROM hackerTable WHERE id=?`, [id], (err, row) => {
+            QRCode.toDataURL('I am a pony!', function (err, url) {
+                console.log(url)
+                res.render('editHacker', { hacker: row[0], hackerURL: url })
+            })
+        }
+    )
+})
+
+router.post('/hacker/:id/update', (req, res) => {
+    const id = req.params.id
+    const firstName = req.body.first_name
+    const lastName = req.body.last_name
+    const email = req.body.hacker_email
+    console.log(firstName)
+    db.run(
+        `UPDATE hackerTable
+        SET firstName=?, lastName=?, email=?
+        WHERE id=?`, [firstName, lastName, email, id], () => {
+            res.redirect("/table/hackerTable")
+        }
+    )
+})
+
+router.get('/event/:id/delete', (req, res) => {
+    const id = req.params.id
+    db.run(`DELETE FROM eventTable WHERE id=?`, [id], () => {
+        res.redirect('/table/eventTable')
+    })
 })
 
 module.exports = router;
